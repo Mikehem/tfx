58da4cbbfbb438684cd66db9d1a5b46205543ed3
jiyongjung <jiyongjung@google.com>
2020-11-24 01:17:18 -0800

    Adds proto <-> json(str/dict) serialization wrapper for consistent behavior.
    All serialization will
     - ignore unknown fields
     - preserve proto field name
     - sort field keys.
    
    PiperOrigin-RevId: 344009589

--- tfx/utils/proto_utils_test.py
+++ tfx/utils/proto_utils_test.py
@@ -30,6 +30,39 @@ class ProtoUtilsTest(tf.test.TestCase):
             'tfx/utils/testdata/foo.proto'
         })
 
+  def test_proto_to_json(self):
+    proto = foo_pb2.TestProto(
+        option1='hello', option2=2, option3=0.5, integer_list=[42])
+    json_str = proto_utils.proto_to_json(proto)
+    # Checks whether original field name is kept and fields are sorted.
+    self.assertEqual(
+        json_str.replace(' ', '').replace('\n', ''),
+        '{"integer_list":[42],"option1":"hello","option2":2,"option3":0.5}')
+
+  def test_proto_to_json_dict(self):
+    proto = foo_pb2.TestProto(
+        option1='hi', option2=4, option3=0.125, integer_list=[24])
+    json_dict = proto_utils.proto_to_json_dict(proto)
+    # Checks whether original field name is kept and fields are sorted.
+    self.assertEqual(json_dict, {
+        'integer_list': [24],
+        'option1': 'hi',
+        'option2': 4,
+        'option3': 0.125
+    })
+
+  def test_json_to_proto(self):
+    json_str = '{"obsolete_field":2,"option1":"x"}'
+    self.assertEqual(
+        proto_utils.json_to_proto(json_str, foo_pb2.TestProto()),
+        foo_pb2.TestProto(option1='x'))
+
+  def test_json_dict_to_proto(self):
+    json_dict = {'option1': 'y', 'option2': 3, 'option3': 0.25}
+    self.assertEqual(
+        proto_utils.json_dict_to_proto(json_dict, foo_pb2.TestProto()),
+        foo_pb2.TestProto(option1='y', option2=3, option3=0.25))
+
 
 if __name__ == '__main__':
   tf.test.main()
